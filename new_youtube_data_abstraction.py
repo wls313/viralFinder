bb144962c60c3c1a32e9f2a180532a6fe12440b4
Numple <numple@naver.com>
2026-06-09 01:27:24 +0900

    youtube 데이터 추출 및 예측 모델(JSON 리턴).DB 변환 ver
    - CSV 파일 대신 DB를 사용하는 버전(keyword_id로 구분)
    - 반드시 {DB 변경 코드.txt}의 DB로 변경할 것
    - 리턴값: JSON

--- new_youtube_data_abstraction.py
+++ new_youtube_data_abstraction.py
@@ -1,13 +1,13 @@
 import datetime
 import os
 import time
+import json
 from googleapiclient.discovery import build
 import pandas as pd
 import pymysql
 
 from key_setting import youtube_api_key, host_ip, user_value, password_value, database_name
 import words_extractor
-#import xgboost_interpretation
 import random_forest_interpretation
 
 # 테스트용 옵션
@@ -21,31 +21,51 @@ youtube = build('youtube', 'v3', developerKey=youtube_api_key)
 MAX_VIDEOS = 100
 MAX_COMMENTS = 100
 WEAK = 7
-DEFAULT_KEYWORD = "제헌절"
+DEFAULT_KEYWORD = "단소살인마"
 
 # 시간
 now_time = datetime.datetime.now(datetime.timezone.utc)
 measurement_time = (now_time - datetime.timedelta(days=WEAK))
 measurement_time_iso = measurement_time.strftime('%Y-%m-%dT%H:%M:%SZ')
 
+def get_db_connection():
+    return pymysql.connect(host=host_ip, user=user_value, password=password_value, database=database_name,
+                           charset='utf8mb4')
+
+# 키워드 ID 조회 및 Insert
+def get_keyword_id(keyword):
+    conn = get_db_connection()
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT keyword_id FROM analysis_keyword WHERE target_keyword=%s", (keyword,))
+            keyword_result = cursor.fetchone()
+            if keyword_result:
+                return keyword_result[0]
+
+            cursor.execute("INSERT INTO analysis_keyword (target_keyword) VALUES (%s)", (keyword,))
+            conn.commit()
+            return cursor.lastrowid
+    finally:
+        conn.close()
+
+
 # 블랙리스트 데이터 가져오기
 def get_blacklist():
-    videos_blacklist = set()
     comments_blacklist = set()
-
     try:
-        with pymysql.connect(host=host_ip, user=user_value, password=password_value, database=database_name, charset='utf8mb4') as conn:
-            with conn.cursor() as cursor:
-                cursor.execute('select type, identify_num from blacklist')
-                for row in cursor.fetchall():
-                    if row[0] == 'video':
-                        videos_blacklist.add(row[1])
-                    elif row[0] == 'comment':
-                        comments_blacklist.add(row[1])
+        conn = get_db_connection()
+        with conn.cursor() as cursor:
+            cursor.execute('select type, identify_num from blacklist')
+            for row in cursor.fetchall():
+                if row[0] == 'comment':
+                    comments_blacklist.add(row[1])
     except Exception as e:
         print(f"오류-블랙리스트를 가져오는데 실패했습니다 : {e}")
+    finally:
+        if 'conn' in locals() and conn.open:
+            conn.close()
 
-    return videos_blacklist, comments_blacklist
+    return comments_blacklist
 
 # 오늘 올라온 영상 갯수
 def count_videos(keyword):
@@ -69,45 +89,26 @@ def count_videos(keyword):
 
     return total
 
-# 댓글 수
-def comment_count(video_ids):
-    print("오늘 작성된 총 댓글 수를 집계합니다...")
-    comments_count_dict = {}
-
-    for i in range(0, len(video_ids), 50):
-        chunk = video_ids[i:i + 50]
-        try:
-            response = youtube.videos().list(
-                id=','.join(chunk),
-                part='statistics',
-            ).execute()
-
-            for item in response.get('items', []):
-                video_id = item['id']
-                total_comments = int(item.get('statistics', {}).get('commentCount', 0))
-                comment_count_dict[video_id] = total_comments
-
-        except Exception as e:
-            print(f"오류-댓글 집계 중 오류가 발생했습니다! : {e}")
-
-    return comment_count_dict
-
 # 영상 탐색
-def search_videos(keyword, exclude_ids=None, max_result = 150):
+def search_videos(keyword, keyword_id, exclude_ids=None, max_result = 150):
     print(f"오늘 업로드 된 {keyword}와 관련된 영상을 탐색합니다...")
 
     if exclude_ids is None:
         exclude_ids = set()
 
     video_ids = []
-    file_dir = os.path.dirname(os.path.abspath(__file__))
-    tracking_file = os.path.join(file_dir, f'youtube_{keyword}.csv')
+    conn = get_db_connection()
 
-    if os.path.exists(tracking_file):
-        df = pd.read_csv(tracking_file)
-        if '영상id' in df.columns:
-            video_ids = df['영상id'].dropna().tolist()
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT DISTINCT video_id FROM youtube_video WHERE keyword_id=%s", (keyword_id,))
+            existing_ids = [row[0] for row in cursor.fetchall()]
+            video_ids.extend(existing_ids)
             print(f"기존에 추적 중인 영상 {len(video_ids)}개를 불러옵니다...")
+    except Exception as e:
+        print(f"DB 조회 오류: {e}")
+    finally:
+        conn.close()
 
     new_video_ids = []
     next_page_token = None
@@ -151,15 +152,12 @@ def search_videos(keyword, exclude_ids=None, max_result = 150):
     print(f"총 {len(video_ids)}개의 영상을 찾았습니다...")
 
     final_videos = (video_ids + new_video_ids)[:MAX_VIDEOS]
-
-    # 다음을 위해 추적 파일 업데이트
-    pd.DataFrame({'영상id': final_videos}).to_csv(tracking_file, index=False, encoding='utf-8-sig')
     print(f'총 {len(final_videos)}개의 영상을 확정했습니다!')
 
     return final_videos
 
 
-def video_stats(video_ids, record_date):
+def video_stats(video_ids, record_date, keyword_id):
     print("수집된 영상의 조회수/좋아요 수 수집하는 중...")
     videos_data_list = []
 
@@ -175,11 +173,12 @@ def video_stats(video_ids, record_date):
                 video_id = item['id']
                 stats = item.get('statistics', {})
                 videos_data_list.append({
-                    '측정기간' : record_date,
-                    '영상id' : video_id,
-                    '일일 조회수': int(stats.get('viewCount', 0)),
-                    '일일 좋아요수': int(stats.get('likeCount', 0)),
-                    '일일 총댓글수': int(stats.get('commentCount', 0))
+                    'record_date' : record_date,
+                    'video_id' : video_id,
+                    'keyword_id' : keyword_id,
+                    'daily_view_count': int(stats.get('viewCount', 0)),
+                    'daily_like_count': int(stats.get('likeCount', 0)),
+                    'daily_comment_count': int(stats.get('commentCount', 0))
                 })
         except Exception as e:
             print(f"오류-영상 데이터를 저장하는 도중 에러가 발생했습니다 : {e}")
@@ -220,14 +219,12 @@ def search_comment(video_id, record_date, keyword, comments_blacklist=None, max_
                    break
 
                comments_data.append({
-                   '측정기간': record_date,
-                   '영상id': video_id,
-                   '댓글 번호': item['id'],
-                   '캡션명': comment.get('authorDisplayName', '').strip(),
-                   '댓글 내용': comment.get('textDisplay', ''),
-                   '좋아요': int(comment.get('likeCount', 0)),
-                   '작성 날짜': published_at,
-                   '광고여부': 0
+                   'record_date': record_date,
+                   'video_id': video_id,
+                   'caption_name': comment.get('authorDisplayName', '').strip(),
+                   'content': comment.get('textDisplay', ''),
+                   'like_count': int(comment.get('likeCount', 0)),
+                   'is_ad': 0
                })
 
                if len(comments_data) >= max_result:
@@ -244,65 +241,98 @@ def search_comment(video_id, record_date, keyword, comments_blacklist=None, max_
    if not comments_data:
        return []
 
-   comments_text_list = [c['댓글 내용'] for c in comments_data]
+   comments_text_list = [c['content'] for c in comments_data]
    print(f"댓글{len(comments_text_list)}개 판독 시작...")
 
    batch_results = words_extractor.AD_search(comments_text_list, keyword)
    time.sleep(4)
 
    for i in range(len(comments_data)):
-       if comments_data[i]['캡션명'] in comments_blacklist:
-           comments_data[i]['광고여부'] = 1
-       else:
-           comments_data[i]['광고여부'] = batch_results[i]
+       caption = comments_data[i]['caption_name']
+       is_blacklist = False
+
+       for black_user in comments_blacklist:
+           if black_user in caption:
+               is_blacklist = True
+               break
+
+   if is_blacklist:
+       comments_data[i]['is_ad'] = 1
+       print(f"{caption}을 블랙리스트에 저장했습니다...")
+   else:
+       comments_data[i]['is_ad'] = batch_results[i]
 
    return comments_data
 
-# csv에 업데이트
-def data_upload_to_csv(filename, data_list):
+# video 데이터를 db에 업데이트
+def video_upload_to_db(data_list, keyword_id):
+    if not data_list:
+        return 0
+
+    conn = get_db_connection()
+    try:
+        with conn.cursor() as cursor:
+           cursor.execute("SELECT MAX(update_count) FROM youtube_video WHERE keyword_id=%s", (keyword_id,))
+           results = cursor.fetchone()[0]
+           current_update = (results if results else 0) + 1
+
+           for data in data_list:
+               data['update_count'] = current_update
+
+           keys = ",".join(data_list[0].keys())
+           vals = ",".join(["%s"] * len(data_list[0]))
+           sql = f"INSERT INTO youtube_video ({keys}) VALUES ({vals})"
+           cursor.executemany(sql, [tuple(data.values()) for data in data_list])
+        conn.commit()
+        return current_update
+    except Exception as e:
+        print(f"오류-영상을 DB에 저장하는 중 에러가 발생했습니다 : {e}")
+        return 0
+    finally:
+        conn.close()
+
+# comment 데이터를 db에 업데이트
+def comment_upload_to_db(data_list, update_count):
     if not data_list:
         return
-    df = pd.DataFrame(data_list)
-    file_exists = os.path.exists(filename)
-    last_updated = 0
 
-    if file_exists:
-        try:
-            df_before = pd.read_csv(filename)
-            if '업데이트 횟수' in df_before.columns:
-                last_updated = df_before['업데이트 횟수'].max()
-        except pd.errors.EmptyDataError:
-            pass
+    conn = get_db_connection()
+    try:
+        with conn.cursor() as cursor:
+            for data in data_list:
+                data['update_count'] = update_count;
 
-    current_update = last_updated + 1
-    df.insert(0, '업데이트 횟수', current_update)
-
-    df.to_csv(filename, mode='a', header=not file_exists, index=False, encoding='utf-8-sig')
-    return current_update
+            keys = ",".join(data_list[0].keys())
+            vals = ",".join(["%s"] * len(data_list[0]))
+            sql = f"INSERT INTO video_comment_analysis ({keys}) VALUES ({vals})"
+            cursor.executemany(sql, [tuple(data.values()) for data in data_list])
+        conn.commit()
+    except Exception as e:
+        print(f"오류-댓글을 DB에 저장하는 중 에러가 발생했습니다 : {e}")
+    finally:
+        conn.close()
 
 # main
 def run_search(keyword):
     record_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-    # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    video_csv = f'videos_{keyword}.csv'
-    comments_csv = f'comments_{keyword}.csv'
-
     today_video_count = count_videos(keyword)
     print(f"측정 완료! {keyword}에 대해 오늘 하루동안 올라온 영상은 {today_video_count}개 입니다.")
-    print(f"'{keyword}' 데이터를 수집합니다...")
+
+    keyword_id = get_keyword_id(keyword)
+    print(f"'{keyword}(keyword_id: {keyword_id})' 데이터를 수집합니다...")
 
     # 블랙 리스트 확인
-    videos_blacklist, comments_blacklist = get_blacklist()
-    target_ids = search_videos(keyword, exclude_ids=videos_blacklist, max_result=MAX_VIDEOS)
+    comments_blacklist = get_blacklist()
+    target_ids = search_videos(keyword, keyword_id, exclude_ids=None, max_result=MAX_VIDEOS)
 
     if not target_ids:
         print("수집할 영상이 없어 종료됩니다.")
-        return 0
+        return {"status": "ERROR", "message": "수집할 영상이 없습니다."}
 
     print("영상 데이터를 수집 및 csv에 업데이트 하는 중...")
-    videos_data = video_stats(target_ids, record_date)
-    current_updated = data_upload_to_csv(video_csv, videos_data)
+    videos_data = video_stats(target_ids, record_date, keyword_id)
+    current_updated = video_upload_to_db(videos_data, keyword_id)
 
     # 댓글 데이터 수집
     comments_data = []
@@ -320,7 +350,7 @@ def run_search(keyword):
         comments_data.extend(single_video_comments)
 
     print("댓글 데이터를 수집 및 csv에 업데이트 하는 중...")
-    data_upload_to_csv(comments_csv, comments_data)
+    comment_upload_to_db(comments_data, current_updated)
 
     result_json = {
         "status": "success",
--- random_forest_interpretation.py
+++ random_forest_interpretation.py
@@ -1,40 +1,74 @@
 import pandas as pd
 import joblib
+import pymysql
+from Scripts.new_youtube_data_abstraction import DEFAULT_KEYWORD
+
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.model_selection import train_test_split
+from key_setting import host_ip, user_value, password_value, database_name
+
+
+
+def get_db_connection():
+    return pymysql.connect(host=host_ip, user=user_value, password=password_value, database=database_name, charset='utf8mb4')
 
 def viral_interpretation(keyword):
     print("\n바이럴 판독 시작")
-    video_csv = f'videos_{keyword}.csv'
-    comments_csv = f'comments_{keyword}.csv'
+    conn = get_db_connection()
 
     try:
-        df_videos = pd.read_csv(video_csv)
-        df_comments = pd.read_csv(comments_csv)
-    except FileNotFoundError:
-        print(f"{keyword}의 CSV 파일이 존재하지 않습니다.")
-        return
+        with conn.cursor() as cursor:
+            cursor.execute("select keyword_id from analysis_keyword where target_keyword=%s",(keyword,))
+            results = cursor.fetchone()
+            if not results:
+                print(f"오류-DB에 {keyword} 데이터가 존재하지 않습니다.")
+                return {"conclusion": "데이터 없음"}
+            keyword_id = results[0]
 
-    comments_sum = df_comments.groupby("영상id").agg(
-        댓글수=('댓글 번호', 'count'),
-        댓글_좋아요_총합=('좋아요', 'sum'),
-        광고로_의심되는_댓글수=('광고여부', 'sum')
-    ).reset_index()
+            cursor.execute("select MAX(update_count) from youtube_video where keyword_id=%s",(keyword_id,))
+            max_update_results = cursor.fetchone()
+            data_gradient = max_update_results[0] if max_update_results[0] else 0
 
-    data_gradient = df_videos['업데이트 횟수'].max()
-    df_latest = df_videos[df_videos['업데이트 횟수'] == data_gradient]
-    df_first = df_videos[df_videos['업데이트 횟수'] == 1]
+            if data_gradient < 2:
+                print(f"오류-데이터가 부족합니다.")
+                return {"conclusion": "데이터 부족"}
 
-    df_Aoi = pd.merge(df_latest[['영상id', '일일 조회수']],
-                      df_first[['영상id', '일일 조회수']],
-                      on='영상id', suffixes=('_최신', '_초기'), how='left')
+        query_latest = f"SELECT video_id, daily_view_count, daily_like_count, daily_comment_count FROM youtube_video WHERE keyword_id={keyword_id} AND update_count={data_gradient}"
+        df_latest = pd.read_sql(query_latest, conn)
+
+        query_first = f"SELECT video_id, daily_view_count FROM youtube_video WHERE keyword_id={keyword_id} AND update_count=1"
+        df_first = pd.read_sql(query_first, conn)
+
+        query_comments = f"SELECT video_id, analysis_id, like_count, is_ad FROM video_comment_analysis WHERE update_count={data_gradient}"
+        df_comments = pd.read_sql(query_comments, conn)
+
+    except Exception as e:
+        print(f"오류-DB에 {keyword} 데이터를 가져오는 중 에러가 발생했습니다 : {e}")
+        return {"conclusion": "DB 로드 중 에러 발생"}
+
+    finally:
+        conn.close()
+
+    if df_comments.empty:
+        comments_sum = pd.DataFrame(columns=['video_id', '댓글수', '댓글_좋아요_총합', '광고로_의심되는_댓글수'])
+
+    else:
+        comments_sum = df_comments.groupby("video_id").agg(
+            댓글수=('analysis_id', 'count'),
+            댓글_좋아요_총합=('like_count', 'sum'),
+            광고로_의심되는_댓글수=('is_ad', 'sum')
+        ).reset_index()
+
+    df_Aoi = pd.merge(df_latest[['video_id', 'daily_view_count']],
+                      df_first[['video_id', 'daily_view_count']],
+                      on='video_id', suffixes=('_최신', '_초기'), how='left')
     # suffixes: 이름표를 추가로 붙이는 속성
 
-    df_Aoi['일일 조회수_초기'] = df_Aoi['일일 조회수_초기'].fillna(0)
-    df_Aoi['조회수 증가량'] = df_Aoi['일일 조회수_최신'] - df_Aoi['일일 조회수_초기']
+    df_Aoi['daily_view_count_초기'] = df_Aoi['daily_view_count_초기'].fillna(0)
+    df_Aoi['조회수 증가량'] = df_Aoi['daily_view_count_최신'] - df_Aoi['daily_view_count_초기']
 
-    df_merged = pd.merge(df_latest, comments_sum, on='영상id', how='left')
-    df_merged = pd.merge(df_merged, df_Aoi[['영상id', '조회수 증가량']], on='영상id', how='left')
+    df_merged = pd.merge(df_latest, comments_sum, on='video_id', how='left')
+    df_merged = pd.merge(df_merged, df_Aoi[['video_id', '조회수 증가량']], on='video_id', how='left')
     df_merged = df_merged.fillna(0)
 
     # 바이럴 점수 임시 계산식
@@ -43,13 +77,13 @@ def viral_interpretation(keyword):
                           (df_merged['광고로_의심되는_댓글수'] * 10)
 
 
-    features = ['일일 조회수', '일일 좋아요수', '댓글수', '댓글_좋아요_총합', '광고로_의심되는_댓글수', '조회수 증가량']
+    features = ['daily_view_count', 'daily_like_count', '댓글수', '댓글_좋아요_총합', '광고로_의심되는_댓글수', '조회수 증가량']
     x = df_merged[features]
     y = df_merged['바이럴 점수']
 
     if len(df_merged) < 2:
         print("오류-학습할 데이터의 개수가 너무 적습니다")
-        return
+        return {"conclusion": "학습 데이터가 부족합니다."}
 
     X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
@@ -63,11 +97,14 @@ def viral_interpretation(keyword):
     total_data_gradient = df_merged['조회수 증가량'].sum()
     total_suspect_ad = df_merged['광고로_의심되는_댓글수'].sum()
 
+    viral_probability = max(0.0, min((keyword_viral_score / 6000.0) * 100, 100.0))
+
     results = {
         'keyword': keyword,
         'total_data_gradient': float(total_data_gradient),
         'total_suspect_ad': int(total_suspect_ad),
         'keyword_viral_score': float(keyword_viral_score),
+        'viral_probability_percentage': f"{round(viral_probability, 1)}%",
         'conclusion': ""
     }
 
@@ -81,4 +118,4 @@ def viral_interpretation(keyword):
     return results
 
 if __name__ == '__main__':
-    viral_interpretation('dummy')
+    viral_interpretation(DEFAULT_KEYWORD)
