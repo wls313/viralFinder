4d790e9bbf46044445f57094f921b750e1005c29
Numple <numple@naver.com>
2026-09-11 21:38:05 +0900

    Delete src directory

--- src/api/progressApi.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const getProgress = async () => {
-  const response = await axios.get(
-    "http://localhost:8000/progress"
-  );
-
-  return response.data;
-};
